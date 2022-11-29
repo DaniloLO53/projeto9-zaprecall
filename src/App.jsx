e750fcbbebd6e7cf7df09d870d47072fa4a4a747
Danilo Leão <olin_ad@hotmail.com>
2022-11-29 11:01:00 -0300

    initial config with css and images

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import styled from 'styled-components';
 import cards from './utils/cards';
 
+import './assets/css/reset.css';
+
 function App() {
   console.log(cards);
 
@@ -14,11 +16,15 @@ function App() {
 }
 
 const StyledGreatContainer = styled.div`
+  background-color: #FB6B6B;
+  width: 100vw;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #fb6b6b;
-  font-family: 'Recursive', sans-serif;
+  margin: 0px;
+  padding: 0px;
+  padding-bottom: 200px;
 `;
 
 // App.PropTypes = {
